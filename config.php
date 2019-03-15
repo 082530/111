8c9681e101003926f981f7458b2193409fc9a98a
ruidong <crd1309399974@163.com>
2019-03-15 11:47:59 +0800

    ''

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+$gl=(isset($_COOKIE['gl']) && $_COOKIE['gl'])?$_COOKIE['gl']:'US';
+define('ROOT_PART', Root_part());
+define('APIKEY', 'AIzaSyDd5SOWHpLfT_sar0-012EIMzTLBHRSb3c');
+define('GJ_CODE', $gl);
+define('SITE_NAME', 'my-video');
+define('TITLENAME', 'my-video');
+define('EN2DEKEY', 'AnQUP1XDEF87ewemhuYG35e67CtzDSh4');
+define('EMAIL', 'crd1309399974@163.com');
+?>
